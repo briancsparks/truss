f9232371d6cc102f241cbc30c2182a70db980f7d
Brian C Sparks <briancsparks@gmail.com>
2021-08-12 13:56:02 -0700

    Fixed newline for console

--- src/main/java/net/cdr0/truss/trusses/ConsoleTruss.java
+++ src/main/java/net/cdr0/truss/trusses/ConsoleTruss.java
@@ -3,7 +3,6 @@ package net.cdr0.truss.trusses;
 import net.cdr0.truss.Truss;
 import net.cdr0.truss.kind.LogAttr;
 import net.cdr0.truss.kind.LogItem;
-import sun.rmi.runtime.Log;
 
 public class ConsoleTruss extends Truss {
 
@@ -21,28 +20,28 @@ public class ConsoleTruss extends Truss {
   // ------------------------------------------------------------------------------------------------------------------
   public void addLogItem(LogItem item) {
     if (item == null) { return; }
-    System.out.printf("%s: %s", item.TAG(), item.toString());
+    System.out.printf("%s: %s\n", item.TAG(), item.toString());
   }
 
   // ------------------------------------------------------------------------------------------------------------------
   public void sendLogItem(LogItem item, String s, String s2, String s3, String s4) {
     if (item == null) { return; }
 
-    System.out.printf("%s: LogItem: (%s|%s|%s|%s) -- %s", item.TAG(), s, s2, s3, s4, item.toString());
+    System.out.printf("%s: LogItem: (%s|%s|%s|%s) -- %s\n", item.TAG(), s, s2, s3, s4, item.toString());
   }
 
   // ------------------------------------------------------------------------------------------------------------------
   public void addLogAttr(LogAttr attr) {
     if (attr == null) { return; }
 
-    System.out.printf("%s: %s", attr.TAG(), attr.toString());
+    System.out.printf("%s: %s\n", attr.TAG(), attr.toString());
   }
 
   // ------------------------------------------------------------------------------------------------------------------
   public void sendLogAttr(LogAttr attr, String s, String s2, String s3, String s4) {
     if (attr == null) { return; }
 
-    System.out.printf("%s: LogItem: (%s|%s|%s|%s) -- %s", attr.TAG(), s, s2, s3, s4, attr.toString());
+    System.out.printf("%s: LogItem: (%s|%s|%s|%s) -- %s\n", attr.TAG(), s, s2, s3, s4, attr.toString());
   }
 
 }
